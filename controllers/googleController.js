f101e8e2343c019cac403a94d9e01780a0f9b114
imAnonAmi <mcintireaw@gmail.com>
2020-06-12 10:55:44 -0700

    Tweaked code in currently non-working googleController, so that a local build could be deployed (with errors) for testing.

--- controllers/googleController.js
+++ controllers/googleController.js
@@ -1,6 +1,8 @@
 // https://developers.google.com/books/docs/v1/getting_started
 // API doc for reference
 
+//tweaked code so I can launch locally (with errors) but I still need to flesh this out to make it work after I am done with Step 5.
+
 const axios = require("axios");
 const db = require("../models");
 
